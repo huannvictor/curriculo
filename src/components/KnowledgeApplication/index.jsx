0e80e050f4e371b67be421f73417535ea35a1121
Huann Almeida <huannvictor@gmail.com>
2022-06-01 22:08:45 -0300

    títulos "conhecimentos adquiridos" atualizado

--- src/components/KnowledgeApplication/index.jsx
+++ src/components/KnowledgeApplication/index.jsx
@@ -3,7 +3,7 @@ import { knowledgeList } from "./content"
 export function KnowledgeApplication(){
   return(
     <div className="flex flex-col gap-1 bg-stone-100 rounded-xl py-7 px-9 shadow-md">
-      <h1 className="text-lg text-brand-500 font-bold text-center">CONHECIMENTO PARA ESTA VAGA</h1>
+      <h1 className="text-lg text-brand-500 font-bold text-center">CONHECIMENTOS ADQUIRIDOS</h1>
       <ul className="content">
         {
           knowledgeList.map((item) => {

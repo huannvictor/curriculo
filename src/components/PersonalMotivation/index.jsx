570214f2c84cdd9ffad58bb8b2349359b4e5bc5c
Huann Almeida <huannvictor@gmail.com>
2022-06-01 22:07:45 -0300

    motivação pessoal atualizada

--- src/components/PersonalMotivation/index.jsx
+++ src/components/PersonalMotivation/index.jsx
@@ -3,8 +3,9 @@ export function PersonalMotivation() {
     <div className="flex flex-col gap-1 bg-stone-100 rounded-xl py-7 px-9 shadow-md">
       <h1 className="text-lg text-brand-500 font-bold text-center">MOTIVAÇÃO PESSOAL</h1>
       <div className="content text-justify indent-2">
-        <p>Programar tem sido uma descoberta desafiadora e instigante. Sempre gostei da área administrativa, mas não me desafia mais.</p>
-        <p>Após pesquisar por pouco mais de um ano, também escutando amigos próximos, vi que a área de Desenvolvimento é um ótimo ponto de partida para esta mudança. Tenho muita vontade em adquirir novas experiências, contribuir com uma equipe focada e estar em uma área que proporcione uma melhor qualidade de vida a mim e minha família.</p>
+        <p>Em uma pesquisa sobre realização profissional e mudança de carreira em 2022, durante a pandemia para ser mais exato, vi que a área de Desenvolvimento é um ótimo ponto de partida. Meus a amigos e pessoas próximas me falaram que eu "levo jeito". Então comecei a pesquisar e estudar sobre o tema.
+        </p>
+        <p> Tenho muita vontade em adquirir novas experiências, contribuir com uma equipe focada e estar em uma área que proporcione essa mudança de vida.</p>
         </div>
     </div>
   )

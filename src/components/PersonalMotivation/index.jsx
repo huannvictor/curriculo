adba847ee50fb311cc8827a73d1de3587c1da7a2
Huann Almeida <huannvictor@gmail.com>
2022-06-03 22:59:55 -0300

    melhorias no texto de motivação pessoal.

--- src/components/PersonalMotivation/index.jsx
+++ src/components/PersonalMotivation/index.jsx
@@ -1,12 +1,24 @@
 export function PersonalMotivation() {
-  return(
+  return (
     <div className="flex flex-col gap-1 bg-stone-100 rounded-xl py-7 px-9 shadow-md">
-      <h1 className="text-lg text-brand-500 font-bold text-center">MOTIVAÇÃO PESSOAL</h1>
+      <h1 className="text-lg text-brand-500 font-bold text-center">
+        MOTIVAÇÃO PESSOAL
+      </h1>
       <div className="content text-justify indent-2">
-        <p>Em uma pesquisa sobre realização profissional e mudança de carreira em 2020, durante a pandemia para ser mais exato, vi que a área de Desenvolvimento é um ótimo ponto de partida. Meus a amigos e pessoas próximas me falaram que eu "levo jeito". Então comecei a pesquisar e estudar sobre o tema.
+        <p>
+          Partindo de uma necessidade de realização pessoal, durante a pandemia
+          para ser mais exato, me questionei sobre mudança de carreira e entendi
+          que a área de Desenvolvimento seria o ponto de partida. Meus a amigos
+          e pessoas próximas me falaram que eu "levo jeito" para a área. Então
+          comecei a pesquisar e estudar sobre esse tema, e a cada aula, cada
+          desafio fui me apaixonando mais e mais pela carreira Dev.
         </p>
-        <p> Tenho muita vontade em adquirir novas experiências, contribuir com uma equipe focada e estar em uma área que proporcione essa mudança de vida.</p>
-        </div>
+        <p>
+          Tenho muita vontade em adquirir novas experiências, contribuir com uma
+          equipe focada e estar em uma área que proporcione essa mudança de
+          vida.
+        </p>
+      </div>
     </div>
-  )
+  );
 }

27941d58ca0b46e1d2901e0e2f396907d99e1b81
Huann Almeida <huannvictor@gmail.com>
2022-06-03 22:42:40 -0300

    funções separadas em arquivos e importadas.

---
+++ src/components/Studies/functions.js
@@ -0,0 +1,58 @@
+import iconLink from "../../assets/externalLink.svg";
+import certificateIcon from "../../assets/certificate.svg";
+
+export function titleFunction(condition, entity, link, place) {
+  if (condition === true) {
+    return (
+      <span>
+        {entity}
+        <a
+          className="hover:underline hover:underline-offset-4 hover:decoration-brand-500 pl-1"
+          href={link}
+          rel="noreferrer"
+          target="_blank"
+        >
+          {place}
+        </a>
+        <img
+          className="inline w-[9px]"
+          src={iconLink}
+          alt="ícone de link externo"
+        />
+      </span>
+    );
+  } else if (condition !== true) {
+    return (
+      <span>
+        {entity}
+        <a
+          className="hover:underline hover:bg-brand-300 rounded-md inline hover:decoration-brand-500"
+          href={link}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            className="inline px-1"
+            src={certificateIcon}
+            alt="ícone de certificado"
+          />
+        </a>
+        <p>{place}</p>
+      </span>
+    );
+  }
+}
+
+export function subjectFunction(subject) {
+  if (typeof subject === "string") {
+    return <p>{subject}</p>;
+  } else {
+    return (
+      <p>
+        {subject.map(item => {
+          return <li key={Math.random()}>{item}</li>;
+        })}
+      </p>
+    );
+  }
+}
--- src/components/Studies/index.jsx
+++ src/components/Studies/index.jsx
@@ -1,64 +1,7 @@
 import { studies } from "./content";
-import iconLink from "../../assets/externalLink.svg";
-import certificateIcon from "../../assets/certificate.svg";
+import { titleFunction, subjectFunction } from "./functions";
 
 export function Studies() {
-  function titleFunction(condition, entity, link, place) {
-    if (condition === true) {
-      return (
-        <span>
-          {entity}
-          <a
-            className="hover:underline hover:underline-offset-4 hover:decoration-brand-500 pl-1"
-            href={link}
-            rel="noreferrer"
-            target="_blank"
-          >
-            {place}
-          </a>
-          <img
-            className="inline w-[9px]"
-            src={iconLink}
-            alt="ícone de link externo"
-          />
-        </span>
-      );
-    } else if (condition !== true) {
-      return (
-        <span>
-          {entity}
-          <a
-            className="hover:underline hover:bg-brand-300 rounded-md inline hover:decoration-brand-500"
-            href={link}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              className="inline px-1"
-              src={certificateIcon}
-              alt="ícone de certificado"
-            />
-          </a>
-          <p>{place}</p>
-        </span>
-      );
-    }
-  }
-
-  function subjectFunction(subject) {
-    if (typeof subject === "string") {
-      return <p>{subject}</p>;
-    } else {
-      return (
-        <p>
-          {subject.map(item => {
-            return <li key={Math.random()}>{item}</li>;
-          })}
-        </p>
-      );
-    }
-  }
-
   return (
     <div className="flex flex-col gap-1 bg-stone-100 rounded-xl py-7 px-9 shadow-md">
       <h1 className="text-lg text-brand-500 font-bold text-center">

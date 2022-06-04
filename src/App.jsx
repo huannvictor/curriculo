ba79a39aac2974079e5649b7f12c7f78c0ade32e
Huann Almeida <huannvictor@gmail.com>
2022-06-03 22:59:26 -0300

    alterações na ordem dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { KnowledgeApplication } from "./components/KnowledgeApplication";
 import { LastXP } from "./components/LastXP";
 import { PersonalMotivation } from "./components/PersonalMotivation";
 import { Studies } from "./components/Studies";
-import { BestRepos } from "./components/BestRepos"
+import { BestRepos } from "./components/BestRepos";
 
 export function App() {
   return (
@@ -11,11 +11,11 @@ export function App() {
       <Header className="justify-center sm:max-w-sm" />
       <div className="flex flex-col gap-3 sm:grid-cols-2 sm:flex-row">
         <div className="left flex flex-col gap-3 text-xs sm:max-w-sm">
-          <PersonalMotivation />
-          <LastXP /> 
-          <Studies/>
+          <LastXP />
+          <Studies />
         </div>
         <div className="left flex flex-col gap-3 text-xs sm:max-w-sm">
+          <PersonalMotivation />
           <KnowledgeApplication />
           <BestRepos />
         </div>

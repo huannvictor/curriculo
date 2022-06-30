cfc336129f41417ef954fe2e752459aa7def3cb0
Huann Almeida <huannvictor@gmail.com>
2022-06-29 22:07:40 -0300

    correções

--- src/components/BestRepos/content.js
+++ src/components/BestRepos/content.js
@@ -1,50 +1,56 @@
 export const reposContent = {
-  portfolio:{
+  portfolio: {
     title: "Currículo",
-    mask:"huannvictor/portifolio",
-    link:"https://github.com/huannvictor/portifolio",
-    description: "Este é meu projeto pessoal com intuito de apresentar minhas habilidades e conhecimentos que tenho adquirido em cursos há pouco mais de um ano. Resolvi consolidar esses conhecimentos em algo prático que mostre como tenho avançado. Esse site estático simples é feito utilizando a estrutura de componentização do React, preocupação com a experiência do usuário e responsividade.",
-    tools:"React - Node - TailwindCss - Deploy no Vercel.com",
-    deploy:"https://portifolio-huannvictor.vercel.app/"
+    mask: "huannvictor/resume",
+    link: "https://github.com/huannvictor/resume",
+    description:
+      "Este é meu projeto pessoal com intuito de apresentar minhas habilidades e conhecimentos que tenho adquirido em cursos há pouco mais de um ano. Resolvi consolidar esses conhecimentos em algo prático que mostre como tenho avançado. Esse site estático simples é feito utilizando a estrutura de componentização do React, preocupação com a experiência do usuário e responsividade.",
+    tools: "React - Node - TailwindCss - Deploy no Vercel.com",
+    deploy: "https://resume-huannvictor.vercel.app/",
   },
-  nlwReturnImpulse:{
-    title: "NLW Return Impulse by Rocketseat",
-    mask:"huannvictor/nlwReturnImpulse",
-    link:"https://github.com/huannvictor/nlwReturnImpulse",
-    description: "Nesta Next Level Week aprendi a fazer um botão para um usuário enviar um formulário de feedback, com texto e um print da tela no momento que está sendo enviado. Essa informação vai para um banco de dados e também por email. Além disso, aprendi como fazer deploy usando Vercel.",
-    tools:"Typescript - React - Node - TailwindCss - Deploy no Vercel.com",
-    deploy:"https://nlw-return-impulse-huannvictor.vercel.app/"
+  nlwReturnImpulse: {
+    title: "NLW Return Impulse by RocketSeat",
+    mask: "huannvictor/nlwReturnImpulse",
+    link: "https://github.com/huannvictor/nlwReturnImpulse",
+    description:
+      "Nesta Next Level Week aprendi a fazer um botão para um usuário enviar um formulário de feedback, com texto e um print da tela no momento que está sendo enviado. Essa informação vai para um banco de dados e também por email. Além disso, aprendi como fazer deploy usando Vercel.",
+    tools: "Typescript - React - Node - TailwindCss - Deploy no Vercel.com",
+    deploy: "https://nlw-return-impulse-huannvictor.vercel.app/",
   },
-  nlwTogether:{
-    title: "NLW Together Impulse by Rocketseat",
-    mask:"huannvictor/letmeask",
-    link:"https://github.com/huannvictor/letmeask",
-    description: "Desenvolvi este app na NLW edição Together. Esse app é um ambiente para Q&A onde usuários conectam com suas contas Google, fazem perguntas e tem suas repostas. Esses dados são armazenados no Firebase e retroalimenta o app.",
-    tools:"Typescript - React - Node - Sass - Firebase com deploy",
-    deploy:"https://letmeask-cfc63.web.app/"
+  nlwTogether: {
+    title: "NLW Together Impulse by RocketSeat",
+    mask: "huannvictor/letmeask",
+    link: "https://github.com/huannvictor/letmeask",
+    description:
+      "Desenvolvi este app na NLW edição Together. Esse app é um ambiente para Q&A onde usuários conectam com suas contas Google, fazem perguntas e tem suas repostas. Esses dados são armazenados no Firebase e retroalimenta o app.",
+    tools: "Typescript - React - Node - Sass - Firebase com deploy",
+    deploy: "https://letmeask-cfc63.web.app/",
   },
-  scrimbaProjectTravelJournal:{
+  scrimbaProjectTravelJournal: {
     title: "React page by Scrimba",
-    mask:"huannvictor/scrimba-project-travel-journal",
-    link:"https://github.com/huannvictor/scrimba-project-travel-journal",
-    description: "Neste projeto simples aprendi a fazer uma página consumindo dados de uma API. A estrutura foi com base no design site da Arbnb",
-    tools:"React - Node- Deploy no Vercel.com",
-    deploy:"https://scrimba-project-travel-journal.vercel.app/"
+    mask: "huannvictor/scrimba-project-travel-journal",
+    link: "https://github.com/huannvictor/scrimba-project-travel-journal",
+    description:
+      "Neste projeto simples aprendi a fazer uma página consumindo dados de uma API. A estrutura foi com base no design site da Arbnb",
+    tools: "React - Node- Deploy no Vercel.com",
+    deploy: "https://scrimba-project-travel-journal.vercel.app/",
   },
-  wordGame:{
+  wordGame: {
     title: "Word Game by Ania Kubów",
-    mask:"huannvictor/word-game",
-    link:"https://github.com/huannvictor/word-game",
-    description: "Game feito seguindo o tutorial do canal Code with Ania Kubów onde consumi informações vinda de uma API dentro do marketplace RapidApi.com. Nesse word game simples podemos associar termos, contabilizando uma pontuação em até 10 níveis de dificuldade.",
-    tools:"React - Node",
-    deploy:""
+    mask: "huannvictor/word-game",
+    link: "https://github.com/huannvictor/word-game",
+    description:
+      "Game feito seguindo o tutorial do canal Code with Ania Kubów onde consumi informações vinda de uma API dentro do marketplace RapidApi.com. Nesse word game simples podemos associar termos, contabilizando uma pontuação em até 10 níveis de dificuldade.",
+    tools: "React - Node",
+    deploy: "",
   },
-  CryptoDashboard:{
+  CryptoDashboard: {
     title: "Crypto Dashboard by Ania Kubów",
-    mask:"huannvictor/crypto-dashboard",
-    link:"https://github.com/huannvictor/crypto-dashboard",
-    description: "Dashboard feita seguindo o tutorial do canal Code with Ania Kubów onde consumi informações de duas APIs do marketplace RapidApi.com: Alpha Vantage (para converter as moedas com valores atualizados) e Crypto News (onde encontramos notícias do universo crypto). Nessa aplicação simples podemos converter valores e ver as principais notícias do universo de crypto moedas",
-    tools:"React - Node",
-    deploy:""
-  }
-}
+    mask: "huannvictor/crypto-dashboard",
+    link: "https://github.com/huannvictor/crypto-dashboard",
+    description:
+      "Dashboard feita seguindo o tutorial do canal Code with Ania Kubów onde consumi informações de duas APIs do marketplace RapidApi.com: Alpha Vantage (para converter as moedas com valores atualizados) e Crypto News (onde encontramos notícias do universo crypto). Nessa aplicação simples podemos converter valores e ver as principais notícias do universo de crypto moedas",
+    tools: "React - Node",
+    deploy: "",
+  },
+};

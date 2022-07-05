c56791785048013c90b7599d354aaf4a8cbfe40c
Huann Almeida <huannvictor@gmail.com>
2022-07-05 14:58:54 -0300

    correção

--- src/components/Studies/content.js
+++ src/components/Studies/content.js
@@ -6,44 +6,46 @@
 // }
 
 export const studies = {
-  softwareEngineer:{
-    title:"BACHAREL EM ENGENHARIA DE SOFTWARE",
-    entity:"ANHANGUERA PITÁGORAS AMPLI",
-    place:"SÃO PAULO/PB",
+  softwareEngineer: {
+    title: "BACHAREL EM ENGENHARIA DE SOFTWARE",
+    entity: "ANHANGUERA PITÁGORAS AMPLI",
+    place: "SÃO PAULO/SP",
     isVirtual: false,
-    link:"https://drive.google.com/file/d/1cjcEADNPj4I5Q-4wKnw3PKYZYNP2BL4v/view?usp=sharing",
-    subject:"Durante os 3 anos de curso, estarei em contato com conteúdos como Algoritmos e Programação Estruturada, Arquitetura e Organização de Computadores, Análise e Modelagem de Sistemas, Engenharia de Requisitos e Arquitetura de Software para possibilitar a execução de tarefas com maior eficiência operacional."
+    link: "https://drive.google.com/file/d/1cjcEADNPj4I5Q-4wKnw3PKYZYNP2BL4v/view?usp=sharing",
+    subject:
+      "Durante os 3 anos de curso, estarei em contato com conteúdos como Algoritmos e Programação Estruturada, Arquitetura e Organização de Computadores, Análise e Modelagem de Sistemas, Engenharia de Requisitos e Arquitetura de Software para possibilitar a execução de tarefas com maior eficiência operacional.",
   },
-  managementBachelor:{
-    title:"BACHAREL EM ADMINISTRAÇÃO DE EMPRESAS",
-    entity:"UNIESP",
-    place:"CABEDELO/PB",
+  managementBachelor: {
+    title: "BACHAREL EM ADMINISTRAÇÃO DE EMPRESAS",
+    entity: "UNIESP",
+    place: "CABEDELO/PB",
     isVirtual: false,
-    link:"https://drive.google.com/file/d/1B7GyTKgtWsba_S9LWh5dIuR8hptHIioZ/view?usp=sharing",
-    subject:"(Incompleto) aguardando defesa do TCC. \nFormação voltada para o mercado, desenvolvendo habilidades como visão de negócios, técnicas e comportamento de liderança, gerir os mais variados recursos de uma empresa como financeiro, humano e material."
+    link: "https://drive.google.com/file/d/1B7GyTKgtWsba_S9LWh5dIuR8hptHIioZ/view?usp=sharing",
+    subject:
+      "(Incompleto) aguardando defesa do TCC. \nFormação voltada para o mercado, desenvolvendo habilidades como visão de negócios, técnicas e comportamento de liderança, gerir os mais variados recursos de uma empresa como financeiro, humano e material.",
   },
-  fullstackJavascriptDeveloper:{
-    title:"PROGRAMADOR FULLSTACK JAVASCRIPT",
-    entity:"ONEBITCODE",
-    place:"onebitcode.com",
+  fullstackJavascriptDeveloper: {
+    title: "PROGRAMADOR FULLSTACK JAVASCRIPT",
+    entity: "ONEBITCODE",
+    place: "onebitcode.com",
     isVirtual: true,
-    link:"https://programador.onebitcode.com",
-    subject:[
+    link: "https://programador.onebitcode.com",
+    subject: [
       "Sass + Bootstrap",
       "Javascript",
       "Typescript",
       "React + Node",
       "SQL + Mongo DB",
-      "Git + Github"
-    ]
+      "Git + Github",
+    ],
   },
-  discoverRocketseat:{
-    title:"TRILHA DISCOVER ROCETSEAT",
-    entity:"ROCKETSEAT",
+  discoverRocketseat: {
+    title: "TRILHA DISCOVER ROCETSEAT",
+    entity: "ROCKETSEAT",
     isVirtual: true,
-    place:"rocketseat.com",
-    link:"https://www.rocketseat.com.br/discover",
-    subject:[
+    place: "rocketseat.com",
+    link: "https://www.rocketseat.com.br/discover",
+    subject: [
       "Hardware, software, firmware e drivers",
       "Propriedades e função de memória RAM, HD/SSD e mais",
       "Pensamento computacional e lógico",
@@ -53,15 +55,15 @@ export const studies = {
       "Programação orientada a objetos",
       "Front-end com foco em HTML, CSS, JavaScript e DOM",
       "Terminal, HTTP a fundo",
-    ]
+    ],
   },
-  learnReact:{
-    title:"LEARN REACT",
-    entity:"SCRIMBA",
-    place:"scrimba.com",
+  learnReact: {
+    title: "LEARN REACT",
+    entity: "SCRIMBA",
+    place: "scrimba.com",
     isVirtual: true,
-    link:"https://scrimba.com/learn/learnreact",
-    subject:[
+    link: "https://scrimba.com/learn/learnreact",
+    subject: [
       "ES6 classes",
       "State",
       "The constructor method",
@@ -71,6 +73,6 @@ export const studies = {
       "componentWillUnmount",
       "Lifecycle methods",
       "this.setState()",
-    ]
-  }
-}
+    ],
+  },
+};

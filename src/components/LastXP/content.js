64894a4007ae90a70e13c32d2e9415ca538604be
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2024-02-01 14:26:11 -0300

    Update content.js

--- src/components/LastXP/content.js
+++ src/components/LastXP/content.js
@@ -1,10 +1,28 @@
 export const xpTitle = "ULTIMAS EXPERIÊNCIAS";
 
 export const xpContent = {
+  TechLead: {
+    position: "TECH LEAD FRONTEND",
+    company: "Skill Labs",
+    period: "janeiro 2024 - atualmente",
+    description:
+      "Enquanto Tech Lead Front-End na Skill Labs, uma inovadora startup dedicada a recrutar profissionais de tecnologia para participar de projetos voluntários, lidero e oriento uma equipe de desenvolvedores front-end em iniciativas que visam aprimorar habilidades técnicas e promover a colaboração. Meu papel envolve a gestão de projetos, a implementação de melhores práticas de desenvolvimento front-end e a promoção de um ambiente colaborativo para alcançar nossos objetivos.",
+    softSkills:
+      "Motivação, aprendizagem contínua, trabalho em equipe, comunicação, adaptabilidade e resiliência",
+  },
+  DevFrontEndJr: {
+    position: "DESENVOLVEDOR FRONTEND",
+    company: "Skill Labs",
+    period: "agosto 2023 - janeiro 2024",
+    description:
+      "Como Desenvolvedor Front-End Júnior na Skill Labs, mergulho de cabeça em projetos inovadores, aplicando minha paixão pela tecnologia para criar experiências de usuário envolventes. Trabalhando em equipe, busco constantemente aprender e aprimorar minhas habilidades, contribuindo ativamente para o desenvolvimento de interfaces intuitivas e funcionais. Na Skill Labs participamos de projetos voluntários que ampliam nossos horizontes e consolidam uma base sólida para minha jornada profissional.",
+    softSkills:
+      "Motivação, aprendizagem contínua, trabalho em equipe, comunicação, adaptabilidade e resiliência",
+  },
   FreelanceFullStackJr: {
     position: "DESENVOLVEDOR FULLSTACK JUNIOR",
     company: "",
-    period: "dezembro 2022 - Atualmente",
+    period: "dezembro 2022 - atualmente",
     description:
       "Como aprendiz de desenvolvedor fullstack júnior em transição de carreira, estou aprendendo habilidades fundamentais em programação, tecnologias web e frameworks para me tornar um desenvolvedor completo. Tenho trabalhado em projetos pessoais e colaborado em outros para adquirir experiência prática e estou empenhado em aprimorar minhas habilidades para contribuir para a indústria de tecnologia.",
     softSkills:
@@ -13,7 +31,7 @@ export const xpContent = {
   SalesAssistant: {
     position: "ASSISTENTE DE VENDAS",
     company: "EDITORA CONSTRUIR",
-    period: "maio 2018 - Atualmente",
+    period: "maio 2018 - atualmente",
     description:
       "Auxiliar os supervisores regionais através criação e tratamento de relatórios e planilhas e executando funções administrativas.",
     softSkills: "Trabalho em equipe, proatividade, inovação.",
